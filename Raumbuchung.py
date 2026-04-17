42544dbd5f1b9b5a978d18ea1fcbb5be114b55f5
wolkenfrei <78296366+wolkenfrei@users.noreply.github.com>
2026-04-17 20:23:30 +0200

    Raumbuchung.py

--- Raumbuchung.py
+++ Raumbuchung.py
@@ -11,5 +11,22 @@
 import streamlit as st
 
 # Block 1 - Titel und erste Anzeige (zeigt die Grundoberfläche der App)
-st.title("Raumbuchung")
-st.write("Das ist ein Raumbuchungssystem")
+st.set_page_config(page_title="Wolkis Raumbuchungssystem") # Tab Namen angepasst
+st.title("Raumbuchung") # Titel der App
+st.write("Das ist ein Raumbuchungssystem") # Beschreibung
+
+# Block 2 - Daten
+raeume = ["Raum A", "Raum B", "Raum C", "Raum D"] # Grundlage! Liste mit allen verfügbaren Räumen
+
+plaetze = {
+    "Raum A": ["Platz 1 (Fenster)", "Platz 2 (Fenster)", "Platz 3"],
+    "Raum B": ["Platz 1 (Fenster)", "Platz 2 (Fenster)", "Platz 3", "Platz 4"],
+    "Raum C": ["Platz 1 (Fenster)"],
+    "Raum D": ["Platz 1 (Fenster)", "Platz 2 (Fenster)", "Platz 3", "Platz 4"]
+} # Jedem Raum wird eine Liste von Plätzen zugeordnet (Dictionary)
+
+# Block 3 - Eingaben
+ausgewaehlter_raum = st.selectbox("Bitte Raum auswählen:", raeume) #Selectbox - Auswahl eines Raums aus der Liste. Ergebnis wird in de
+
+# Block 5 - Ausgabe
+st.write("Ausgewählter Raum:", ausgewaehlter_raum) # Ausgabe was ausgewählt worden ist.
